4d3d21f35bd97bb86f3ae4b86d95b0a96205426f
Frank Marra <frankrmarra@gmail.com>
2024-01-31 20:31:04 -0500

    removed daily update

--- netlify/functions/scheduled-deploy.js
+++ netlify/functions/scheduled-deploy.js
@@ -1,19 +1,19 @@
-import fetch from 'node-fetch'
-import { schedule } from '@netlify/functions'
+// import fetch from 'node-fetch'
+// import { schedule } from '@netlify/functions'
 
-const BUILD_HOOK =
-  'https://api.netlify.com/build_hooks/656e1f768cfecc08d5b7184b'
+// const BUILD_HOOK =
+//   'https://api.netlify.com/build_hooks/656e1f768cfecc08d5b7184b'
 
-const handler = schedule('0 5 * * *', async () => {
-  await fetch(BUILD_HOOK, {
-    method: 'POST'
-  }).then((response) => {
-    console.log('Build hook response: ', response)
-  })
+// const handler = schedule('0 5 * * *', async () => {
+//   await fetch(BUILD_HOOK, {
+//     method: 'POST'
+//   }).then((response) => {
+//     console.log('Build hook response: ', response)
+//   })
 
-  return {
-    statusCode: 200
-  }
-})
+//   return {
+//     statusCode: 200
+//   }
+// })
 
-export { handler }
+// export { handler }
